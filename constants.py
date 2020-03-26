337cc3a5b5b8a732feeb74892e1980ad96d3eea8
Hastings Greer <hastings@hgreer.com>
2020-03-25 22:36:05 -0400

    bigger batches

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-volumes_path = "C:/Users/hastings.local/Desktop/converted_from_dicom/converted_from_dicom/"
+volumes_path = "/media/data/hastings/converted_from_dicom/"
 
-annotations_path = "C:/d/ct-angledslice-align/train_set/"
+annotations_path = "/media/data/hastings/ct-angledslice-align/train_set/"
